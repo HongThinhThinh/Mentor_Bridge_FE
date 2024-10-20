6b135b95d8f4a40d3cab1b588d54c6a31a712900
baotran1311 <baotran13112003@gmail.com>
2024-10-20 16:50:07 +0700

    Feat/Tran: Add Room Chat

---
+++ src/pages/roomChat/index.tsx
@@ -0,0 +1,38 @@
+import "./RoomChat.scss";
+import { useStateValue } from "../../context/stateProvider";
+import { useMediaQuery } from "react-responsive";
+import { Outlet, useParams } from "react-router-dom";
+import ChatList from "../../components/molecules/chatList";
+import FormSearchFriends from "../../components/molecules/formSearchFriends";
+
+function RoomChat() {
+  const { theme, showSearchFriends, showChatList } = useStateValue();
+  const isQuery = useMediaQuery({ maxWidth: 800 });
+  const { param } = useParams();
+
+  return (
+    <div
+      className="roomChat"
+      style={{
+        backgroundColor: theme ? "#202020" : "#fff",
+        color: theme ? "#fff" : "#202020",
+      }}
+    >
+      {isQuery ? (
+        <>
+          {showChatList && <ChatList />}
+          {!showChatList && <Outlet />}
+          {showSearchFriends && <FormSearchFriends />}
+        </>
+      ) : (
+        <>
+          <ChatList />
+          <Outlet />
+          {showSearchFriends && <FormSearchFriends />}
+        </>
+      )}
+    </div>
+  );
+}
+
+export default RoomChat;

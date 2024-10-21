b7d349463f87e325c58408243f683981a5d4cd14
NguyenHongThinh-2003 <ocbu0212@gmail.com>
2024-10-21 12:00:54 +0700

    Fix/Thinh: Reconfig Router Page

--- src/utils/navigate.tsx
+++ src/utils/navigate.tsx
@@ -1,6 +1,10 @@
 import { useNavigate } from "react-router-dom";
 import { Role } from "../constants/role";
-import { ADMIN_ROUTES, MENTOR_ROUTES } from "../constants/routes";
+import {
+  ADMIN_ROUTES,
+  MENTOR_ROUTES,
+  STUDENT_ROUTES,
+} from "../constants/routes";
 
 export const useNavigateByRole = () => {
   const navigate = useNavigate(); // sử dụng hook useNavigate bên trong custom hook
@@ -11,7 +15,7 @@ export const useNavigateByRole = () => {
         navigate(`/${ADMIN_ROUTES.ADMIN}`);
         break;
       case Role.STUDENT:
-        navigate(`/${ADMIN_ROUTES.ADMIN}`);
+        navigate(`/${STUDENT_ROUTES.STUDENT}/${STUDENT_ROUTES.STUDENT_PAGE}`);
         break;
       case Role.MENTOR:
         navigate(`/${MENTOR_ROUTES.MENTOR}/${MENTOR_ROUTES.MENTOR_PAGE}`);

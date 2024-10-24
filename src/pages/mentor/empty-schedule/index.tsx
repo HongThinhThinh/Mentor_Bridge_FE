d67fa62d741a72ebd5f8364f3190c599ed47c164
Nguyen Ngoc Y Nhi <ynhiworkplace308@gmail.com>
2024-10-24 21:36:15 +0700

    Feat/Nhi: Implement Empty Schedule Page

---
+++ src/pages/mentor/empty-schedule/index.tsx
@@ -0,0 +1,11 @@
+import VerticalScheduler from "../../../components/molecules/vertical-scheduler";
+
+
+
+const EmptySchedulePage = () => {
+  return (<div className="mt-8 empty-schedule">
+  <VerticalScheduler />
+  </div>);
+};
+
+export default EmptySchedulePage;

801e8375fb1ef3907d7c16e34c564d847f98456e
HongThinhThinh <ocbu0212@gmail.com>
2024-10-25 01:47:50 +0700

    Feat/Thinh: Add Utils DateFormat

--- src/utils/dateFormat.ts
+++ src/utils/dateFormat.ts
@@ -3,3 +3,7 @@ import moment from "moment";
 export const formatDateToDDMMYY = (date: Date | string): string => {
   return moment(date).format("DD-MM-YYYY");
 };
+
+export const formatHours = (date: Date | string): string => {
+  return moment(date).format("HH:mm");
+};

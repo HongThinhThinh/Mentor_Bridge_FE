104be715187836288a263d3d887c36912cc21c12
NguyenHongThinh-2003 <ocbu0212@gmail.com>
2024-10-24 15:57:10 +0700

    Feat/Thinh: UI Topic Service

--- src/pages/admin/manage-topic/index.tsx
+++ src/pages/admin/manage-topic/index.tsx
@@ -1,9 +1,10 @@
-import { Form, Input } from "antd";
+import { Form, Input, Button } from "antd";
 import TextArea from "antd/es/input/TextArea";
 import DashboardTemplate, {
   Column,
 } from "../../../components/templates/dashboard-template";
-import { TOPIC, TOPIC_API } from "../../../constants/endpoints";
+import { TOPIC_API } from "../../../constants/endpoints";
+import { downloadBase64File } from "../../../utils/dowloadBase64File";
 
 function ManageTopic() {
   const title = "Topic";
@@ -13,13 +14,46 @@ function ManageTopic() {
       dataIndex: "name",
       key: "name",
     },
-
     {
       title: "Description",
       dataIndex: "description",
       key: "description",
     },
+    {
+      title: "Assets",
+      dataIndex: "files",
+      key: "files",
+      render: (assets: { name: string; content: string }[]) => (
+        <div className="flex justify-center items-center gap-2">
+          {assets?.map((asset, index) => (
+            <Button
+              key={index}
+              type="link"
+              onClick={() => downloadBase64File(asset.content, asset.name)}
+            >
+              {asset.name}
+            </Button>
+          ))}
+        </div>
+      ),
+    },
+    {
+      title: "Actions",
+      dataIndex: "id",
+      key: "actions",
+      render: (id: string, record: any) => (
+        <div style={{ display: "flex", gap: "10px" }}>
+          <Button type="primary" onClick={() => handleAccept(id)}>
+            Accept
+          </Button>
+          <Button type="primary" danger onClick={() => handleReject(id)}>
+            Reject
+          </Button>
+        </div>
+      ),
+    },
   ];
+
   const formItems = (
     <>
       <Form.Item label="Enter name" name={"name"}>
@@ -30,9 +64,11 @@ function ManageTopic() {
       </Form.Item>
     </>
   );
+
   return (
     <div>
       <DashboardTemplate
+        isCustom
         apiURI={TOPIC_API.TOPIC}
         formItems={formItems}
         title={title}

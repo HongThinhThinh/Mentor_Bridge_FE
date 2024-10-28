2bf6bac826d9671e17832b5ad5aca0d348a76d8c
baotran1311 <baotran13112003@gmail.com>
2024-10-29 00:56:34 +0700

    Feat/Tran: Add Detail Team

--- src/components/molecules/team-section/index.tsx
+++ src/components/molecules/team-section/index.tsx
@@ -1,8 +1,9 @@
-import { memo } from "react";
+import { memo, useState } from "react";
 import { User } from "../../../model/user";
 import "./index.scss";
 import { Topic } from "../../../model/topic";
 import { HiDotsHorizontal } from "react-icons/hi";
+import TeamDetail from "../../organisms/team-detail";
 interface GroupItemProps {
   index?: number;
   id?: string;
@@ -15,20 +16,41 @@ interface GroupItemProps {
 // Sửa lỗi ở đây, truyền props cho hàm của React.memo
 const GroupItem: React.FC<GroupItemProps> = memo(
   ({ index, id, code, createdAt, userTeams, topic }) => {
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => {
+      setOpen(true);
+    };
+
+    const handleClose = () => {
+      setOpen(false);
+    };
     return (
-      <li className="group-item">
-        <span className="index">{index}</span>
-        <div className="code">
-          <span className="title">{code}</span>
-          <span className="title">{" (" + userTeams?.length} thành viên)</span>
-        </div>
-        <div className="content">
-          <span className="title">{topic?.name}</span>
-        </div>
-        <div>
-          <HiDotsHorizontal />
-        </div>
-      </li>
+      <>
+        <li className="group-item">
+          <span className="index">{index}</span>
+          <div className="code">
+            <span className="title">{code}</span>
+            <span className="title">
+              {" (" + userTeams?.length + " thành viên)"}{" "}
+            </span>
+          </div>
+          <div className="content">
+            <span className="title">{topic?.name}</span>
+          </div>
+          <div>
+            <HiDotsHorizontal onClick={handleOpen} />
+          </div>
+        </li>
+
+        <TeamDetail
+          isOpen={open}
+          onCancel={handleClose}
+          topic={topic}
+          userTeams={userTeams}
+          code={code}
+        />
+      </>
     );
   }
 );

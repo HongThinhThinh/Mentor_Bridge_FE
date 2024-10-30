fb4693edbdeb07e648af624389f50cd95db63fc4
HongThinhThinh <ocbu0212@gmail.com>
2024-10-30 13:22:16 +0700

    Feat/Thinh: Admin Chart Bar

--- src/pages/admin/manage-overview/index.tsx
+++ src/pages/admin/manage-overview/index.tsx
@@ -1,96 +1,99 @@
-import React from "react";
+import { Card, Col, Row, Statistic } from "antd";
+import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
+import React, { useEffect, useState } from "react";
 import {
-  BarChart,
   Bar,
+  BarChart,
+  CartesianGrid,
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  Tooltip,
   XAxis,
   YAxis,
-  Tooltip,
-  CartesianGrid,
-  Legend,
-  PieChart,
-  Pie,
-  Cell,
 } from "recharts";
+import useAdminService from "../../../services/useAdminService";
+import api from "../../../config/api";
 
-const ManageOverview = () => {
-  // Dữ liệu được truyền vào dưới dạng array
-  const data = [
-    {
-      month: 12,
-      year: 2024,
-      totalRevenue: 926000,
-    },
-    {
-      month: 10,
-      year: 2024,
-      totalRevenue: 426000,
-    },
-    {
-      month: 11,
-      year: 2024,
-      totalRevenue: 226000,
-    },
-    {
-      month: 9,
-      year: 2024,
-      totalRevenue: 126000,
-    },
-    // Bạn có thể thêm dữ liệu cho nhiều tháng khác nếu cần
-  ];
+function Overview() {
+  const [data, setData] = useState([]);
+  const [top5Mentor, seTop5Mentor] = useState([]);
+  const { getStats } = useAdminService();
+  const fetchData = async () => {
+    const response = await getStats();
+    console.log(response);
+    const formatData = response.top5MostBookedMentors.map((item) => ({
+      name: item?.userResponse.fullName,
+      totalBooking: item?.bookingCount,
+    }));
+    seTop5Mentor(formatData);
+    setData(response);
+  };
 
-  // Định dạng dữ liệu để hiển thị tháng theo định dạng dễ hiểu
-  const formattedData = data.map((item) => ({
-    name: `Tháng ${item.month}/${item.year}`, // Hiển thị tháng/năm trên trục X
-    totalRevenue: item.totalRevenue, // Doanh thu để hiển thị trên trục Y
-  }));
-  const data2 = [
-    { name: "Product A", revenue: 1200 },
-    { name: "Product B", revenue: 980 },
-    { name: "Product C", revenue: 750 },
-    { name: "Product D", revenue: 620 },
-    { name: "Product E", revenue: 540 },
-  ];
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: "40px",
-        flexDirection: "column",
-      }}
-    >
-      <>
-        <PieChart width={600} height={400}>
-          <Pie
-            data={data2}
-            dataKey="revenue"
-            nameKey="name"
-            cx="50%"
-            cy="50%"
-            outerRadius={150}
-            fill="#8884d8"
-            label
-          >
-            {data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={COLORS[index]} />
-            ))}
-          </Pie>
-          <Tooltip />
-          <Legend />
-        </PieChart>
-      </>
-      <h3>Doanh thu hàng tháng</h3>
-      <BarChart width={600} height={300} data={formattedData}>
-        <XAxis dataKey="name" stroke="#8884d8" /> {/* Hiển thị tháng/năm */}
-        <YAxis /> {/* Hiển thị trục Y cho doanh thu */}
-        <Tooltip /> Tooltip khi hover vào các thanh
-        <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-        <Bar dataKey="totalRevenue" fill="#8884d8" barSize={30} />
+    <div>
+      <Row gutter={16}>
+        <Col span={12}>
+          <Card bordered={false}>
+            <Statistic
+              title="Total Mentor"
+              value={data?.mentorCount}
+              valueStyle={{
+                color: "#cf1322",
+              }}
+              prefix={<ArrowDownOutlined />}
+              suffix=""
+            />
+          </Card>
+        </Col>
+        <Col span={12}>
+          <Card bordered={false}>
+            <Statistic
+              title="Student Customer"
+              value={data?.studentCount}
+              valueStyle={{
+                color: "#cf1322",
+              }}
+              prefix={<ArrowDownOutlined />}
+              suffix=""
+            />
+          </Card>
+        </Col>
+      </Row>
+      <PieChart width={400} height={200}>
+        <Pie
+          data={data?.topProduct}
+          dataKey="totalSold"
+          nameKey="productName"
+          cx="50%"
+          cy="50%"
+          outerRadius={50}
+          fill="#8884d8"
+          label
+        >
+          {data?.topProduct?.map((item, index) => (
+            <Cell key={index} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Tooltip />
+        <Legend />
+      </PieChart>
+      <BarChart width={500} height={250} data={top5Mentor}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="totalBooking" fill="#8884d8" />
       </BarChart>
     </div>
   );
-};
+}
 
-export default ManageOverview;
+export default Overview;
